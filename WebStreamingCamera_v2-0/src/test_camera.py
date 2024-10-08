1a9e62fc8bf412185f323a506a5e9e53518ae7a6
neighbort <102603275+neighbort@users.noreply.github.com>
2024-10-08 23:48:21 +0900

    Delete WebStreamingCamera_v2-0/src/test_camera.py

--- WebStreamingCamera_v2-0/src/test_camera.py
+++
@@ -1,9 +0,0 @@
-from picamera2 import Picamera2
-
-picam2 = Picamera2()
-
-# Take photos
-picam2.start_and_capture_file("test.jpg")
-
-# Take movies
-picam2.start_and_record_video("test.mp4", duration=5)
