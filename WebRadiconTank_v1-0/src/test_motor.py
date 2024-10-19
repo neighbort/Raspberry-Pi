3969061b1c25985037dcb154b5779876b65fa788
neighbort <102603275+neighbort@users.noreply.github.com>
2024-10-20 01:01:37 +0900

    Update test_motor.py

--- WebRadiconTank_v1-0/src/test_motor.py
+++ WebRadiconTank_v1-0/src/test_motor.py
@@ -10,12 +10,12 @@ def run_dcmotor():
     GPIO.setup(fow, GPIO.OUT)
     GPIO.setup(bcw, GPIO.OUT)
     
-    GPIO.output(fow, 0)
-    GPIO.output(bcw, 1)
-    sleep(1)
-    
     GPIO.output(fow, 1)
     GPIO.output(bcw, 0)
+    sleep(1)
+    
+    GPIO.output(fow, 0)
+    GPIO.output(bcw, 1)
     sleep(1)
         
     GPIO.output(fow, 0)
